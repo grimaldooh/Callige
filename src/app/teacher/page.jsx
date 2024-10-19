d4552ae48e5708af761b163b8a42b3084b6a30fe
grimaldooh <angel.grimaldo@uabc.edu.mx>
2024-10-19 16:33:40 -0700

    si

--- src/app/teacher/page.jsx
+++ src/app/teacher/page.jsx
@@ -113,7 +113,7 @@ const TeachersPage = () => {
                       onClick={() => openListaAsistentesModal(event.id)}
                       className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
                     >
-                      Ver detalles
+                      Lista de asistencia
                     </button>
                   </div>
                 </div>
