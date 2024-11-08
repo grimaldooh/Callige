aad25cf37a7e73979cf7b23214bf6b5bf48758e7
grimaldooh <angel.grimaldo@uabc.edu.mx>
2024-11-07 22:04:54 -0800

    stats fixeadas

--- pages/api/stats.jsx
+++ pages/api/stats.jsx
@@ -4,11 +4,8 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === 'GET') {
-    const token = req.headers.authorization?.split(" ")[1]; // Obtener el token del header
 
-    if (!token) {
-      return res.redirect('/auth/login');
-    }
+  
     const { schoolId } = req.query; // Obtener el schoolId de la query
     console.log('schoolId:', schoolId);
 
